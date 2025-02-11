d5577f499d6b85b9feccf4b16c84b6a71a352f4c
AdrianUrenaMoraga <192288093+AdrianUrenaMoraga@users.noreply.github.com>
2025-02-11 08:12:33 -0600

    fix fechas

--- index.js
+++ index.js
@@ -8,14 +8,15 @@ import {getFirstAndLastDateOfCurrentMonth, logWithTimestamp, logErrorWithTimesta
     
 try{
     //const { firstDate, lastDate } = getFirstAndLastDateOfCurrentMonth();
-
+    /*
     const firstDate = "2024-02-16T00:00:00.000Z";
     const lastDate = "2024-02-20T23:59:59.999Z";
+    */
+
     const date = getDay();
-    /*
     const firstDate = `${date}T00:00:00.000Z`;
     const lastDate = `${date}T23:59:59.999Z`;
-    */
+    
     console.log('First Date:', firstDate);
     console.log('Last Date:', lastDate);
     
